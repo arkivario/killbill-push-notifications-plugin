1c8bc6dcbc241ae8d9b406d950ed1023babf5edd
mitm32 <mitm32@eml.cc>
2022-12-01 18:06:49 +0600

    init healthcheck

---
+++ src/main/java/org.example.killbill.plugin.anvancedpush/PluginHealthcheck.java
@@ -0,0 +1,15 @@
+package org.example.killbill.plugin.anvancedpush;
+
+import org.killbill.billing.osgi.api.Healthcheck;
+import org.killbill.billing.tenant.api.Tenant;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+public class PluginHealthcheck implements Healthcheck {
+
+    @Override
+    public HealthStatus getHealthStatus(@Nullable final Tenant tenant, @Nullable final Map properties) {
+        return HealthStatus.healthy(); //todo implement
+    }
+}
