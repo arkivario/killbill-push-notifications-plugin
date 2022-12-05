476d41f5546c01c95b28ac29c56c8f0b314914fd
mitm32 <mitm32@eml.cc>
2022-12-05 18:21:50 +0600

    init PluginServlet

---
+++ src/main/java/org/example/killbill/plugin/advancedpush/PluginServlet.java
@@ -0,0 +1,13 @@
+package org.example.killbill.plugin.advancedpush;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jooby.mvc.Path;
+
+import javax.inject.Singleton;
+
+@Slf4j
+@Singleton
+@Path("/")
+public class PluginServlet {
+
+}
