309cc67df61ab314655b284e32490dd07a9ce696
mitm32 <mitm32@eml.cc>
2022-12-20 16:31:58 +0600

    + plugin ping request

--- src/main/java/org/example/killbill/plugin/advancedpush/PluginServlet.java
+++ src/main/java/org/example/killbill/plugin/advancedpush/PluginServlet.java
@@ -1,13 +1,33 @@
 package org.example.killbill.plugin.advancedpush;
 
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jooby.mvc.GET;
+import org.jooby.mvc.Local;
 import org.jooby.mvc.Path;
+import org.killbill.billing.tenant.api.Tenant;
 
+import javax.inject.Named;
 import javax.inject.Singleton;
+import java.util.Optional;
 
 @Slf4j
+@NoArgsConstructor
 @Singleton
 @Path("/")
 public class PluginServlet {
 
+    @GET
+    public void ping(@Local @Named("killbill_tenant") final Optional<Tenant> tenant) {
+        log.info("Hello from KillBill Another push notification plugin");
+
+        if(tenant != null && tenant.isPresent() ) {
+            log.info("tenant is available");
+            Tenant t1 = tenant.get();
+            log.info("tenant id:"+t1.getId());
+        }
+        else {
+            log.info("tenant is not available");
+        }
+    }
 }
