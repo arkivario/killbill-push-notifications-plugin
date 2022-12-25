799732f8493a2a7e33721c8fbf094b82ac5e39a1
mitm32 <mitm32@eml.cc>
2022-12-25 18:57:55 +0600

    fix com.google.inject.CreationException

--- src/main/java/org/example/killbill/billing/plugin/notification/push/PluginHealthcheckServlet.java
+++ src/main/java/org/example/killbill/billing/plugin/notification/push/PluginHealthcheckServlet.java
@@ -1,24 +1,23 @@
 package org.example.killbill.billing.plugin.notification.push;
 
+import lombok.RequiredArgsConstructor;
 import org.jooby.Result;
 import org.jooby.mvc.GET;
 import org.jooby.mvc.Local;
 import org.jooby.mvc.Path;
 import org.killbill.billing.tenant.api.Tenant;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
 import java.util.Optional;
 
+@RequiredArgsConstructor(onConstructor_ = {@Inject})
 @Singleton
 @Path("/healthcheck")
 public class PluginHealthcheckServlet extends org.killbill.billing.plugin.core.resources.PluginHealthcheck {
 
     private final PluginHealthcheck healthcheck;
-
-    public PluginHealthcheckServlet(final PluginHealthcheck healthcheck) {
-        this.healthcheck = healthcheck;
-    }
 
     @GET
     public Result check(@Local @Named("killbill_tenant") final Optional<Tenant> tenant) {
