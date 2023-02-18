e206604a85c593ab242cfdafc5aaede7b5354a68
m1kelo <option_surfer_0e@icloud.com>
2023-02-18 21:28:59 +0600

    set up dbi test, + todos

---
+++ src/test/java/org/example/killbill/billing/plugin/notification/push/dao/CallbacksDaoTest.java
@@ -0,0 +1,32 @@
+package org.example.killbill.billing.plugin.notification.push.dao;
+
+import org.killbill.billing.platform.test.PlatformDBTestingHelper;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/*
+* todo: generate ddl script using Liquibase changelog,
+*  place it at target/generated-test-resources/org/killbill/billing/beatrix/ddl-<dbEngineName>.sql
+*  (see org.killbill.billing.platform.test.PlatformDBTestingHelper.executePostStartupScripts())
+* */
+public class CallbacksDaoTest {
+
+    @BeforeSuite(groups = "dbi")
+    public void setupDBI() throws SQLException, IOException {
+        PlatformDBTestingHelper.get().start();
+    }
+
+    @BeforeMethod(groups = "dbi")
+    public void resetDB() throws IOException {
+        PlatformDBTestingHelper.get().getInstance().cleanupAllTables();
+    }
+
+    @AfterSuite(groups = "dbi")
+    public void tearDown() throws IOException {
+        PlatformDBTestingHelper.get().getInstance().stop();
+    }
+}
